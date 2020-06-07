6f04f4de7c7d2be55c0268be448b70b1cc415f27
ashur-k <ashurkanwal@yahoo.com>
2020-06-07 18:33:00 +0000

    CRUD functionality added

---
+++ mongo_project.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pymongo
+import os
+if os.path.exists('env.py'):
+    import env
+
+MONGODB_URI = os.getenv('MONGO_URI')
+DBS_NAME = 'myTestDB'
+COLLECTION_NAME = 'myFirstDB'
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print ('Could not connect to Mongo DB: %s') % e
+
+
+def get_record():
+    print("")
+    first = input("Enter first name >")
+    last = input("Enter last name >")
+
+    try:
+        doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
+    except:
+        print("Error accessing the database")
+    
+    if not doc:
+        print("")
+        print("Error! No Results found.")
+
+    return doc
+
+
+def show_menu():
+    print ('')
+    print ('1. Add a record')
+    print ('2. Find a record by name')
+    print ('3. Edit a record')
+    print ('4. Delete a record')
+    print ('5. Exit')
+
+    option = input('Enter Option: ')
+    return option
+
+
+def add_record():
+    print ("")
+    first = input('Enter first name >')
+    last = input('Enter last name >')
+    dob = input('Enter date of birth >')
+    gender = input('Enter gender >')
+    hair_colour = input('Enter enter hair colour >')
+    occupation = input('Enter occupation >')
+    nationality = input('Enter nationality >')
+
+    new_doc = {
+        'first': first.lower(),
+        'last': last.lower(),
+        'dob': dob,
+        'gender': gender,
+        'hair_colour': hair_colour,
+        'occupation': occupation,
+        'nationality': nationality,
+        }
+
+    try:
+        coll.insert(new_doc)
+        print ('')
+        print ('Document inserted')
+    except:
+        print ('Error accessing database')
+
+
+def find_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ":" + v.capitalize())
+
+
+def edit_record():
+    doc = get_record()
+    if doc:
+        update_doc = {}
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                update_doc[k] = input(k.capitalize() + " [" + v + "] > ")
+                
+                if update_doc[k] == "":
+                    update_doc[k] = v
+        
+        try:
+            coll.update_one(doc, {'$set': update_doc})
+            print("")
+            print("Document updated")
+        except:
+            print("Error accessing the databasae")
+
+def main_loop():
+    while True:
+        option = show_menu()
+        if option == '1':
+            add_record()
+        elif option == '2':
+            find_record()
+        elif option == '3':
+            edit_record()
+        elif option == '4':
+            print ('You have selected option 4')
+        elif option == '5':
+            conn.close()
+            break
+        else:
+            print ('Invalid option')
+        print ("")
+
+
+conn = mongo_connect(MONGODB_URI)
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+main_loop()
